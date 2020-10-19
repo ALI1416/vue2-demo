af67ca736a48ee0f4350668b0c427ccd44392f78
ALI <1416978277@qq.com>
2020-10-19 22:11:29 +0800

    7

---
+++ 03-模块化/02-模块化的雏形/01-b.js
@@ -0,0 +1,7 @@
+var js_b = (function () {
+    var obj = {};
+    var a = 'b';
+    obj.a = a;
+    console.log(js_a.a);
+    return obj;
+})()
---
+++ 03-模块化/03-ES6模块化的使用/01-a.js
@@ -0,0 +1,37 @@
+/* 直接在变量/函数/类前使用export关键词 */
+export let a1 = 'a1';
+export function f1() {
+    console.log('f1');
+}
+export class C1 {
+    f() {
+        console.log('c1');
+    }
+}
+
+/* 批量导出 */
+let a2 = 'a2';
+function f2() {
+    console.log('f2');
+}
+class C2 {
+    f() {
+        console.log('c2');
+    }
+}
+// 在尾部export要导出的对象
+export { a2, f2, C2 }
+
+/* 使用default别人可以自己命名 */
+/* 注意：一个模块里面最多有1个默认值 */
+let a3 = 'a3';
+function f3() {
+    console.log('f3');
+}
+class C3 {
+    f() {
+        console.log('c3');
+    }
+}
+// export后加上default关键字
+export default { a3, f3, C3 }
