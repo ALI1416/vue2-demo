f2ac186d9d1416d2d11bbae7c893a7e68f85ffad
ALI <1416978277@qq.com>
2020-10-23 22:02:00 +0800

    10

--- 05-vueCLI/03-cli3/src/main.js
+++ 05-vueCLI/03-cli3/src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// 构建过程中提示信息
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  // $mount('#app')等同于el:'#app'挂载#app
 }).$mount('#app')
