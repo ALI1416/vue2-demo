001d75c102d5c8b5e3c300dc629860713b731c34
ALI <1416978277@qq.com>
2020-11-02 21:22:07 +0800

    18

--- 06-mall-project/src/views/home/childComps/HomeSwiper.vue
+++ 06-mall-project/src/views/home/childComps/HomeSwiper.vue
@@ -3,22 +3,21 @@
     <swiper class="swiper" :options="swiperOption">
       <swiper-slide v-for="item in banners" :key="item.url">
         <div>
-          <!-- <a :href="item.link"> -->
-          <img :src="item.url" />
-          <!-- </a> -->
+          <a :href="item.link">
+            <img :src="item.url" class="swiper-img" />
+          </a>
         </div>
       </swiper-slide>
-      <div class="swiper-scrollbar"></div>
-      <div class="swiper-button-next"></div>
-      <div class="swiper-button-prev"></div>
+      <div class="swiper-button-prev" slot="button-prev"></div>
+      <div class="swiper-button-next" slot="button-next"></div>
       <div class="swiper-pagination" slot="pagination"></div>
     </swiper>
   </div>
 </template>
 <script>
-// 引入第三方轮播图库 npm install swiper vue-awesome-swiper --save
+// 引入第三方轮播图库 npm install swiper@5 vue-awesome-swiper@4 --save
 import { Swiper, SwiperSlide } from "vue-awesome-swiper";
-import "swiper/swiper-bundle.css";
+import "swiper/css/swiper.css";
 export default {
   name: "HomeSwiper",
   props: {
@@ -37,8 +36,10 @@ export default {
     return {
       swiperOption: {
         loop: true,
+        autoplay: true,
         pagination: {
           el: ".swiper-pagination",
+          clickable: true,
         },
         navigation: {
           nextEl: ".swiper-button-next",
@@ -50,4 +51,11 @@ export default {
 };
 </script>
 <style scoped>
+.swiper-img {
+  width: 100%;
+}
+.swiper-container {
+  --swiper-navigation-color: #fff6;
+  --swiper-pagination-color: #fff8;
+}
 </style>
